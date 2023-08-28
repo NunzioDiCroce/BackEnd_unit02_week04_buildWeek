1348df88a0ff8f8f95d2bd963a689e84c007c5cb
NunzioDiCroce <nunziodavide@gmail.com>
2023-08-28 17:26:05 +0200

    Create TipoSede.java

--- epicEnergy/src/main/java/laCompagniaDelCodice/epicEnergy/entities/Sede.java
+++ epicEnergy/src/main/java/laCompagniaDelCodice/epicEnergy/entities/Sede.java
@@ -7,7 +7,9 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import laCompagniaDelCodice.epicEnergy.enums.TipoSede;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -24,6 +26,14 @@ public class Sede {
 	@Id
 	@GeneratedValue
 	private UUID id;
+
+	private String via;
+	private int civico;
+	private String localita;
+	private String cap;
+
+	@ManyToOne
+	private Cliente cliente;
 
 	@Enumerated(EnumType.STRING)
 	private TipoSede tipoSede;
