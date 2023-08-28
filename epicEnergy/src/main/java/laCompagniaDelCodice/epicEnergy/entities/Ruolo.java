c5df1f22ae30735118b920154c8f6b65064cdbb9
NunzioDiCroce <nunziodavide@gmail.com>
2023-08-28 17:57:12 +0200

    Update application.properties

--- epicEnergy/src/main/java/laCompagniaDelCodice/epicEnergy/entities/Ruolo.java
+++ epicEnergy/src/main/java/laCompagniaDelCodice/epicEnergy/entities/Ruolo.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -29,6 +30,7 @@ public class Ruolo {
 	private Boolean ruoloAmministratore;
 
 	@ManyToMany
+	@JoinTable(name = "Utente_Ruolo")
 	private List<Utente> utente;
 
 }
--- epicEnergy/src/main/java/laCompagniaDelCodice/epicEnergy/entities/Utente.java
+++ epicEnergy/src/main/java/laCompagniaDelCodice/epicEnergy/entities/Utente.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -32,6 +33,7 @@ public class Utente {
 	private String cognome;
 
 	@ManyToMany
+	@JoinTable(name = "Utente_Ruolo")
 	private List<Ruolo> ruolo;
 
 }
