ba2fe10e1f672a42e1fe93b4d2682694f404f0e5
NunzioDiCroce <nunziodavide@gmail.com>
2023-08-28 17:03:43 +0200

    Create Utente.java

---
+++ epicEnergy/src/main/java/laCompagniaDelCodice/epicEnergy/entities/Utente.java
@@ -0,0 +1,36 @@
+package laCompagniaDelCodice.epicEnergy.entities;
+
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Utenti")
+public class Utente {
+
+	@Id
+	@GeneratedValue
+	private UUID id;
+
+	private String username;
+	private String password;
+	private String email;
+	private String nome;
+	private String cognome;
+
+	@ManyToMany
+	private Ruolo ruolo;
+
+}
