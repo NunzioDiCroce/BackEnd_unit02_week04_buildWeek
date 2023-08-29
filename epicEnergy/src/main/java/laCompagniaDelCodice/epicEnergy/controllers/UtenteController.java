ff6c78b66204e40ebb9dd8e50b597c16b12ab275
NunzioDiCroce <nunziodavide@gmail.com>
2023-08-29 10:55:46 +0200

    Create UtenteController.java

---
+++ epicEnergy/src/main/java/laCompagniaDelCodice/epicEnergy/controllers/UtenteController.java
@@ -0,0 +1,10 @@
+package laCompagniaDelCodice.epicEnergy.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/utenti")
+public class UtenteController {
+
+}
