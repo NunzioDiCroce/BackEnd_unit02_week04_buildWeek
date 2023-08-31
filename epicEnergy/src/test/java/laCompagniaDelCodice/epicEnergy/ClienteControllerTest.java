06ab38cd8818a8d7fa68e27480a43e1325b0df4c
NunzioDiCroce <nunziodavide@gmail.com>
2023-08-31 13:12:37 +0200

    Update ClienteControllerTest.java

--- epicEnergy/src/test/java/laCompagniaDelCodice/epicEnergy/ClienteControllerTest.java
+++ epicEnergy/src/test/java/laCompagniaDelCodice/epicEnergy/ClienteControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import laCompagniaDelCodice.epicEnergy.controllers.ClienteController;
 import laCompagniaDelCodice.epicEnergy.services.ClienteService;
 
 //* * * * CLASSE PER TEST CREAZIONE CLIENTE * * * *
@@ -16,6 +17,9 @@ public class ClienteControllerTest {
 
 	@Test
 	public void testCreateCliente() {
+
+		ClienteController clienteController = new ClienteController();
+		clienteController.setClienteService(clienteService);
 
 	}
 
