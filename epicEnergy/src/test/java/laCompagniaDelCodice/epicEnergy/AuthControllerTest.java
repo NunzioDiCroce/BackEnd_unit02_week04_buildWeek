3eea039291096dad81821c8bbfb4dd470bb9a643
NunzioDiCroce <nunziodavide@gmail.com>
2023-08-31 12:30:40 +0200

    Create AuthControllerTest.java

--- epicEnergy/src/test/java/laCompagniaDelCodice/epicEnergy/AuthControllerTest.java
+++ epicEnergy/src/test/java/laCompagniaDelCodice/epicEnergy/AuthControllerTest.java
@@ -1,10 +1,25 @@
 package laCompagniaDelCodice.epicEnergy;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import laCompagniaDelCodice.epicEnergy.security.JWTTools;
+import laCompagniaDelCodice.epicEnergy.services.UtenteService;
 
 //* * * * CLASSE PER TEST AUTHCONTROLLER (LOGIN UTENTE) * * * *
 
 @SpringBootTest
 public class AuthControllerTest {
 
+	@MockBean // ANNOTAZIONE PER CREARE UNA SIMULAZIONE DEL BEAN IN SPRING
+	private UtenteService utenteService;
+
+	@MockBean // ANNOTAZIONE PER CREARE UNA SIMULAZIONE DEL BEAN IN SPRING
+	private JWTTools jwtTools;
+
+	@Test
+	public void testLoginSuccess() {
+
+	}
 }
