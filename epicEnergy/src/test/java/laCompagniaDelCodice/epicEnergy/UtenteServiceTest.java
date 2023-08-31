eeb5ac72d8d48bd89e8209082ded8ccaf591f4ba
NunzioDiCroce <nunziodavide@gmail.com>
2023-08-31 11:40:10 +0200

    Create UtenteServiceTest.java

---
+++ epicEnergy/src/test/java/laCompagniaDelCodice/epicEnergy/UtenteServiceTest.java
@@ -0,0 +1,10 @@
+package laCompagniaDelCodice.epicEnergy;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+// CLASSE PER TESTARE LA CREAZIONE DI UN UTENTE
+
+@SpringBootTest
+public class UtenteServiceTest {
+
+}
