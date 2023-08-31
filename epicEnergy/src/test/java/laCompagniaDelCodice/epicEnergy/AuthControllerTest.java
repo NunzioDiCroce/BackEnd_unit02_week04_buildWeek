945ff33b18767058ce9a2457961e9f35da8242d6
NunzioDiCroce <nunziodavide@gmail.com>
2023-08-31 12:39:24 +0200

    Update AuthControllerTest.java

--- epicEnergy/src/test/java/laCompagniaDelCodice/epicEnergy/AuthControllerTest.java
+++ epicEnergy/src/test/java/laCompagniaDelCodice/epicEnergy/AuthControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import laCompagniaDelCodice.epicEnergy.entities.Utente;
 import laCompagniaDelCodice.epicEnergy.payloads.UtenteLoginPayload;
 import laCompagniaDelCodice.epicEnergy.security.JWTTools;
 import laCompagniaDelCodice.epicEnergy.services.UtenteService;
@@ -22,7 +23,12 @@ public class AuthControllerTest {
 	@Test
 	public void testLoginSuccess() {
 
-		UtenteLoginPayload utenteLoginPayload = new UtenteLoginPayload("user@example.com", "password");
+		UtenteLoginPayload utenteLoginPayload = new UtenteLoginPayload("utenteTest@mail.com", "passwordTest");
+
+		Utente utente = new Utente("utenteTest", "passwordTest", "utenteTest@mail.com", "nomeTest", "cognomeTest",
+				"AMMINISTRATORE");
+
+		String token = "tokenTest";
 
 	}
 }
