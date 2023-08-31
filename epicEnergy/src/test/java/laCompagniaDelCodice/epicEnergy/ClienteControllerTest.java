bdf24a1f93eaf28ddefc346c9c68522221e0ef5b
NunzioDiCroce <nunziodavide@gmail.com>
2023-08-31 14:35:35 +0200

    Update ClienteControllerTest.java

--- epicEnergy/src/test/java/laCompagniaDelCodice/epicEnergy/ClienteControllerTest.java
+++ epicEnergy/src/test/java/laCompagniaDelCodice/epicEnergy/ClienteControllerTest.java
@@ -1,10 +1,19 @@
 package laCompagniaDelCodice.epicEnergy;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.UUID;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import laCompagniaDelCodice.epicEnergy.controllers.ClienteController;
+import laCompagniaDelCodice.epicEnergy.entities.Cliente;
+import laCompagniaDelCodice.epicEnergy.enums.TipoCliente;
 import laCompagniaDelCodice.epicEnergy.services.ClienteService;
 
 //* * * * CLASSE PER TEST CREAZIONE CLIENTE * * * *
@@ -19,7 +28,26 @@ public class ClienteControllerTest {
 	public void testCreateCliente() {
 
 		ClienteController clienteController = new ClienteController();
-		clienteController.setClienteService(clienteService);
+
+		UUID clienteId = UUID.randomUUID();
+		Cliente nuovoCliente = new Cliente();
+		nuovoCliente.setId(clienteId);
+		nuovoCliente.setRagioneSociale("ragioneSocialeTest");
+		nuovoCliente.setPartitaIva("partitaIvaTest");
+		nuovoCliente.setEmail("testMail@mail.com");
+		nuovoCliente.setDataInserimento(new Date());
+		nuovoCliente.setDataUltimoContatto(new Date());
+		nuovoCliente.setFatturaAnnuale(BigDecimal.valueOf(50000));
+		nuovoCliente.setPec("testPec@pec.com");
+		nuovoCliente.setTelefono("testTelefono");
+		nuovoCliente.setEmailContatto("emailContattoTest@email.com");
+		nuovoCliente.setNomeContatto("nomeContattoTest");
+		nuovoCliente.setCognomeContatto("cognomeContattoTest");
+		nuovoCliente.setTelefonoContatto("telefonoContattoTest");
+		nuovoCliente.setTipoCliente(TipoCliente.SPA);
+
+		// DEFINIZIONE COMPORTAMENTO MOCK CLIENTESERVICE
+		when(clienteService.saveCliente(any(Cliente.class))).thenReturn(nuovoCliente);
 
 	}
 
