a7d9062c7513ca54faf5a343f4688d3a7b4ce6f6
NunzioDiCroce <nunziodavide@gmail.com>
2023-08-31 12:05:26 +0200

    Update UtenteServiceTest.java

--- epicEnergy/src/test/java/laCompagniaDelCodice/epicEnergy/UtenteServiceTest.java
+++ epicEnergy/src/test/java/laCompagniaDelCodice/epicEnergy/UtenteServiceTest.java
@@ -1,10 +1,39 @@
 package laCompagniaDelCodice.epicEnergy;
 
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.mockito.Mockito.when;
 
-// CLASSE PER TESTARE LA CREAZIONE DI UN UTENTE
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import laCompagniaDelCodice.epicEnergy.entities.Utente;
+import laCompagniaDelCodice.epicEnergy.payloads.UtenteSavePayload;
+import laCompagniaDelCodice.epicEnergy.services.UtenteService;
+
+// * * * * CLASSE PER TEST METODO CREATE DI UTENTESERVICE (CREAZIONE UTENTE) * * * *
 
 @SpringBootTest
 public class UtenteServiceTest {
+
+	@MockBean // ANNOTAZIONE PER CREARE UNA SIMULAZIONE DEL BEAN IN SPRING
+	private UtenteService utenteService;
+
+	@Test
+	public void testCreaUtente() {
+
+		// DATI NECESSARI PER CREARE UN UTENTE
+		UtenteSavePayload payload = new UtenteSavePayload("utenteTest", "passwordTest", "utenteTest@mail.com",
+				"nomeTest", "cognomeTest", "AMMINISTRATORE");
+
+		// DATI ATTESI UTENTE
+		Utente nuovoUtente = new Utente("utenteTest", "passwordTest", "utenteTest@mail.com", "nomeTest", "cognomeTest",
+				"AMMINISTRATORE");
+
+		// DEFINIZIONE COMPORTAMENTO MOCK UTENTESERVICE
+		when(utenteService.create(payload)).thenReturn(nuovoUtente);
+
+		Utente utenteCreato = utenteService.create(payload);
+
+	}
 
 }
