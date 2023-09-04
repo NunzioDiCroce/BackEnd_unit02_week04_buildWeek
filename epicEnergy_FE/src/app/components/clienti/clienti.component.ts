a26e725691dde4a8b0ac0b913f13f7760487e0e0
Luke932 <126018318+Luke932@users.noreply.github.com>
2023-09-04 09:29:03 +0200

    funzionamento get da FE

--- epicEnergy_FE/src/app/components/clienti/clienti.component.ts
+++ epicEnergy_FE/src/app/components/clienti/clienti.component.ts
@@ -1,11 +1,9 @@
 import { HttpErrorResponse } from '@angular/common/http';
-import { Component, OnInit, Pipe } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { AuthData } from 'src/app/auth/auth-data.interface';
 import { AuthService } from 'src/app/auth/auth.service';
-import { TokenInterceptor } from 'src/app/auth/token.interceptor';
 import { Cliente } from 'src/app/model/clienti.interface';
-import { Utente } from 'src/app/model/utente.interface';
 import { EpicEnergyServiceService } from 'src/app/service/epic-energy-service.service';
 
 @Component({
@@ -14,20 +12,16 @@ import { EpicEnergyServiceService } from 'src/app/service/epic-energy-service.se
   styleUrls: ['./clienti.component.scss']
 })
 export class ClientiComponent implements OnInit {
-  subClienti!: Subscription;
   user!: AuthData | null;
-  clienti: Cliente[] | undefined;
-  sub!: Subscription;
+  clienti: Cliente[] = []; // Inizializza l'array clienti
   token!: string | null;
 
   constructor(private clientiSrv: EpicEnergyServiceService, private authSrv: AuthService) { }
 
   ngOnInit(): void {
-
-
     this.authSrv.user$.subscribe((_user) => {
-        this.token = localStorage.getItem('utente');
-        console.log(this.token)
+      this.token = localStorage.getItem('utente');
+      console.log(this.token);
       this.user = _user;
       console.log(this.user);
 
@@ -37,17 +31,19 @@ export class ClientiComponent implements OnInit {
       }
     });
 
-    this.subClienti = this.clientiSrv.getAllClienti().subscribe(
-        (clienti: Cliente[]) => {
-          this.clienti = clienti;
+    this.clientiSrv.getAllClienti().subscribe(
+      (data: any) => {
+        // Verifica se data è un array prima di assegnarlo a clienti
+        if (Array.isArray(data.content)) {
+          this.clienti = data.content;
           console.log(this.clienti);
-          console.log(5);
-        },
-        (error: HttpErrorResponse) => {
-
-          console.error('Errore nella richiesta HTTP:',error);
+        } else {
+          console.error('I dati ricevuti non sono un array:', data);
         }
-      );
-
+      },
+      (error: HttpErrorResponse) => {
+        console.error('Errore nella richiesta HTTP:', error);
+      }
+    );
   }
 }
